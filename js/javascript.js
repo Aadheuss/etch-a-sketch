b2b932a4202e52e5bee8ec3722adfb6a9c83a8d9
Aadheuss <aadheusx@gmail.com>
2022-06-24 12:58:39 +0700

    add interactive greeting function

--- js/javascript.js
+++ js/javascript.js
@@ -20,14 +20,35 @@ other possibilities including float clear, inline block, and css grid.*/
 
 //start coloring after the first click and stop when the mouse button is released.
 function startColor(passIn) {
-  passIn.forEach(grid => grid.addEventListener('mousedown', changeColor.bind(null, grid), false));
+  passIn.forEach(grid => grid.addEventListener('mousedown', changeColor.bind(null, grid)));
 }
 //make a function that will change the color of the grid div if the use click and hover the mouse
 //our goal is to make the change feel responsive but not too responsive that the color will spill
 //add event listener to the grid div
 //hovering addEventListener hint onMouseEnter onMouseLeave
 //change color 
-function changeColor(changeMe, passMe) {
+function changeColor(changeMe) {
  changeMe.classList.add('changed');
 }
+
+const today = new Date();
+const hourNow = today.getHours();
+let greeting;
+if (hourNow >= 18) {
+  greeting = 'Good evening!';
+} else if (hourNow >= 16) {
+  greeting = 'Good afternoon!';
+} else if (hourNow >= 12) {
+  greeting = 'Good day!';
+} else if (hourNow >= 6) {
+  greeting = 'Good morning!';
+} else {
+  greeting = 'Good night!';
+}
+
+const showMe = document.querySelector('.right');
+const welcome = document.createElement('div');
+welcome.textContent = greeting + ' Welcome to the draw zone!';
+showMe.appendChild(welcome);
+
 
