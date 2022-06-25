67a79ebe542298739105cd511d61cf0979e1a21e
Aadheuss <aadheusx@gmail.com>
2022-06-25 08:45:59 +0700

    Add function that change color on button click
    I make a function that will change the color property when different
    button is clicked, and I also add a function that will add button effect
    transition on click.

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,9 @@
 const gridContainer = document.querySelector('.grid');
+const buttons = Array.from(document.querySelectorAll('.buttons button'));
+let color = '#f7f4e9;';
 //select the grid container
 createGrid(16);
+//make 16 rows, and 16 column 
 
 function createGrid(num) {
   num = num * num;
@@ -9,40 +12,56 @@ function createGrid(num) {
     grid.classList.add('square')
     grid.style.border = '1px solid black';
     gridContainer.appendChild(grid);
+//Create new div elements
   };
-/*create a div with a square shape
-make 16 rows, and 16 column 
-01.possibility no one using css flex display property
-other possibilities including float clear, inline block, and css grid.*/
 }
 
 const grids = Array.from(document.querySelectorAll('.square'));
   grids.forEach(box => box.addEventListener('mousedown', startColor));
+//if the user click it will fire up a function that will start the color change.
 
-//if the user click it will fire up a function that will change the style color attribute
-//after the function called the mousemove will change the color of targeted div.
-
-//start coloring after the first click and stop when the mouse button is released.
 function startColor() {
-  grids.forEach(grid => grid.addEventListener('mousemove', changeColor));
+  let start = grids.forEach(grid => grid.addEventListener('mousemove', changeColor));
   grids.forEach(grid => grid.addEventListener('mouseup', () => {
     grids.forEach(grid => grid.removeEventListener('mousemove', changeColor));
-    console.log('what happen?')
+  
+//start coloring when mouse is moving and stop when the mouse button is released.
     })
   );
 }
-//make a function that will change the color of the grid div if the use click and hover the mouse
-//our goal is to make the change feel responsive but not too responsive that the color will spill
-//add event listener to the grid div
-//hovering addEventListener hint onMouseEnter onMouseLeave
-//change color 
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('chosen');
+}
+
+buttons.forEach(button => button.addEventListener('click', pickColor))
+buttons.forEach(button => button.addEventListener('click', () => {
+  button.classList.add('chosen');
+}))
+buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
+//remove the border change
+
+function pickColor(e) {
+  let targetColor = e.target.className;
+  console.log(color);
+  if (targetColor === 'yellow') {
+    color = '#eba63f';
+  } else if (targetColor === 'green') {
+    color = '#438945';
+  } else if (targetColor === 'blue') {
+    color = '#3cbcc3'
+  } else if (targetColor === 'reset') {
+    grids.forEach(grid => grid.style.backgroundColor = '#f7f4e9');
+    color = '#f7f4e9;';
+  } else {
+    color = color;
+  }
+}
 function changeColor(e) {
  
- e.target.classList.add('changed');
+ e.target.style.backgroundColor = color;
+//change the color of individual grid div
 }
-//If it clicked or hover the color change but how do I change the color?
-//If clicked change individual div
-//individual div class is square let's try to see the event
 
 const today = new Date();
 const hourNow = today.getHours();
@@ -63,5 +82,6 @@ const showMe = document.querySelector('#right');
 const welcome = document.createElement('div');
 welcome.textContent = greeting;
 showMe.insertBefore(welcome, showMe.firstChild);
+//Show interactive greeting message
 
 
